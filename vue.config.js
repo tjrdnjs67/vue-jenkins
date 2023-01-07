58f7fdb589fcd3c18d55026c5e6edfc131815a51
이석원 <tjrdnjs67@naver.com>
2023-01-07 16:06:00 +0900

    2023-01-07

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: path.resolve(
+    __dirname,
+    "dist"
+  ),
+  publicPath : "/dist",
 })
